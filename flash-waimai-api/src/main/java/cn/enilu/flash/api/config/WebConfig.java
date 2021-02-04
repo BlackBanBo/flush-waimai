d2f1474a4831b30710b659a3d3ee3df7c6349965
yuanzhongping <yuanzhongping@bonc.com.cn>
2021-02-04 15:23:44 +0800

    解决跨域访问

--- flash-waimai-api/src/main/java/cn/enilu/flash/api/config/WebConfig.java
+++ flash-waimai-api/src/main/java/cn/enilu/flash/api/config/WebConfig.java
@@ -6,13 +6,15 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * @author ：enilu
  * @date ：Created in 2019/9/7 11:03
  */
 @Configuration
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
     @Bean
     public ConfigurableServletWebServerFactory webServerFactory() {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
@@ -23,5 +25,14 @@ public class WebConfig {
             }
         });
         return factory;
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .maxAge(3600);
+
     }
 }
